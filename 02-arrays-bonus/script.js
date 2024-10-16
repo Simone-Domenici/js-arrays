5669ec4385484a08c42fad2988a114514b66242f
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-16 16:20:32 +0200

    FIX 2nd Bonus exercise

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -24,8 +24,7 @@ console.log(teachers)
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 let longNames = [];
-longNames = teachers.toSorted((a, b) => a.length - b.length)
-longNames.splice(0,3)
+longNames = teachers.filter((word) => word.length >= 5)
 console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
